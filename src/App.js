3841cc27dae7a924a9285ce8c79b5b3657de8044
Vladimir Pavković <vladimirpavk@gmail.com>
2019-10-03 13:15:08 +0200

    +

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+function formatName(user){
+  return user.name + ' ' + user.lastname;
+}
+
+const user1 = {
+  name: 'Pavle',
+  lastname: 'Pavković'
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        { formatName(user1) }
       </header>
     </div>
   );
