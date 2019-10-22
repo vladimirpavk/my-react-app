2c0d0751e029366bef9acfd8daa4529c932a7dc9
Vladimir Pavković <vladimirpavk@gmail.com>
2019-10-22 12:59:24 +0200

    +

--- src/MyListComponent/MyList.js
+++ src/MyListComponent/MyList.js
@@ -12,27 +12,92 @@ class MyList extends Component{
     }
 
     onClearArray = ()=>{
-        this.setState({
-            list : []
+        this.setState((state)=>{
+            return {
+                list : [],
+                value :''
+            }
         });
     }
 
-    onResetArray = ()=>{
+    /* onClearArray = ()=>
+        this.setState({
+            list : []            
+        }); */
+
+    /* onResetArray = ()=>{
         this.setState(
             INITIAL_STATE
         );
+    } */
+
+    onResetArray = ()=>{
+        this.setState(
+            (oldState)=>{
+                return  INITIAL_STATE;
+            }           
+        );
     }
 
-    onAddValue = ()=>{
+   /*  onAddValue = ()=>{   
         this.setState({
-            list : [...this.state.list, this.state.value]
+            list : [...this.state.list, this.state.value]            
         });
-        console.log(this.state);
+    } */
+    onAddValue = ()=>{   
+        this.setState(state=>
+        {
+            const newList = [...this.state.list, this.state.value];
+        
+            return {
+                list : newList,
+                value: '',
+            };
+        });
     }
     
-    onValueChanged = (event)=>{
+    /* onValueChanged = (event)=>{
         this.setState({
-            value : event.target.value
+            value : +event.target.value
+        });
+    } */
+
+    onValueChanged = (event)=>{
+        //must use event persist becouse arrow function is in different context
+        event.persist();
+        this.setState(
+            (oldState)=>{
+                return {
+                    value : +event.target.value
+                }
+            }
+        )
+        /* this.setState({
+            value : +event.target.value
+        }); */
+    }
+
+    elementClicked = (elementIndex)=>{
+        //console.log('Element clicked...', e);
+        //const listElementIndex = this.state.list.indexOf(listElement);
+        const newList = this.state.list.slice();
+        newList[elementIndex] += 1;
+
+        //console.log(newList);
+
+        this.setState({
+            list : newList
+        });
+    }
+
+    removeElement = (index, e)=>{
+        const partOne = this.state.list.slice(0, index);
+        const partTwo = this.state.list.slice(index+1);
+
+        const newList = partOne.concat(partTwo);
+        
+        this.setState({
+            list : newList
         });
     }
 
@@ -43,7 +108,15 @@ class MyList extends Component{
                 <ul>
                     {
                         (this.state.list || []).map(
-                            (listElement, index)=><li key={index}>Element {listElement}</li>
+                            (listElement, index)=>{
+                                return(
+                                    <div key={1000*index}>
+                                        <li>Element {listElement}</li>
+                                        <button onClick={(e)=>this.elementClicked(index)}>Increase by 1</button>
+                                        <button onClick={(e)=>this.removeElement(index)}>Remove</button>
+                                    </div>
+                                );
+                            }
                         )
                     }
                 </ul>
@@ -56,12 +129,14 @@ class MyList extends Component{
                     Reset Array
                 </button>
                 
-                <input
-                    type="text"
-                    value={this.state.value} 
-                    onChange={this.onValueChanged}
-                />
-                <button onClick={this.onAddValue}>Add value</button>
+                <div>
+                    <input
+                        type="text"
+                        value={this.state.value} 
+                        onChange={this.onValueChanged}
+                    />
+                    <button onClick={this.onAddValue}>Add value</button>
+                </div>               
 
             </div>           
         );
